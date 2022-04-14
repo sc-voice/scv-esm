1f1934597105b1bd94ced108eb4dc930abf1f6af
Karl Lew <karl@firepick.org>
2022-04-14 09:57:57 -0700

    v1.3.0 npmignore

---
+++ scripts/js/suidmap.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const Axios = require('axios');
+const { logger } = require('log-instance');
+const { Memoizer } = require('memo-again');
+const {
+    BilaraData,
+    Publication,
+    BilaraPathMap,
+    ExecGit,
+} = require('scv-bilara');
+const APP_DIR = path.join(__dirname, '..', '..');
+const API_DIR = path.join(APP_DIR, 'api');
+const SRC_DIR = path.join(APP_DIR, 'src');
+const SRC_SUIDMAPJSON = path.join(SRC_DIR, 'auto', 'suidmap.json');
+
+logger.logLevel = 'info';
+
+(async function(){ try {
+    let execGit = new ExecGit({
+        repo: 'https://github.com/ebt-site/ebt-data.git',
+        logger,
+    });
+    let bilaraData = await new BilaraData({
+        name: 'ebt-data',
+        branch: 'published',
+        execGit,
+    }).initialize(true);
+    let publication = await new Publication().initialize();
+    let bilaraPathMap = await new BilaraPathMap({publication}).initialize();
+    let suidMap = await bilaraPathMap.buildSuidMap();
+    let suidJson = JSON.stringify(suidMap, null, '\t');
+    await fs.promises.writeFile(SRC_SUIDMAPJSON, suidJson);
+} catch(e) {
+    logger.warn(e);
+}})();
